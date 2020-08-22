15745eadf26c29b80a243c27a1191bb69f8a6333
keshabdora <kesabdora333@gmail.com>
2020-08-22 13:14:51 +0530

    intil commit

---
+++ src/main/java/com/example/demo/SpringBootJenkinsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsExampleApplication.class, args);
+	}
+
+}
